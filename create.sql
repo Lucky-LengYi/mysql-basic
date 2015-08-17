34071aa7e4c110b9070fd930ef8234a0f13a50b2
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-08-17 15:50:02 +0800

    add the insert.sql

--- create.sql
+++ create.sql
@@ -2,36 +2,40 @@ CREATE DATABASE IF NOT EXISTS TWDemo DEFAULT CHARSET utf8 COLLATE utf8_general_c
 
 use TWDemo;
 
-CREATE TABLE student
-(
-    id int(10) auto_increment not null primary key,
-    student_id int(20) not null,
-    name varchar(20) not null,
-    sex varchar(20) not null,
-    class_name varchar(20) not null
-);
-
 CREATE TABLE class
 (
-    id int(10) auto_increment not null primary key,
-    class_id int(20) not null,
+    class_id int not null,
     class_name varchar(20) not null,
     college varchar(20) not null,
-    department varchar(20) not null
+    department varchar(20) not null,
+    PRIMARY KEY (class_id)
+);
+
+CREATE TABLE student
+(
+    student_id int not null,
+    name varchar(20) not null,
+    sex varchar(20) not null,
+    class_id int not null,
+    PRIMARY KEY (student_id),
+    FOREIGN KEY (class_id) REFERENCES class(class_id)
 );
 
 CREATE TABLE course
 (
-    id int(10) auto_increment not null primary key,
-    course_id int(20) not null,
+    course_id int not null,
     course_name varchar(20) not null,
     start_time varchar(20) not null,
     end_time varchar(20) not null,
-    day varchar(20) not null
+    day varchar(20) not null,
+    PRIMARY KEY (course_id)
 );
 
 CREATE TABLE schedule
 (
-    student_id int(20) not null,
-    course_id int(20) not null
+    student_id int not null,
+    course_id int not null,
+    PRIMARY KEY (course_id),
+    FOREIGN KEY (student_id)  REFERENCES student(student_id),
+    FOREIGN KEY (course_id)  REFERENCES course(course_id)
 );
