2a17fb20f125e113ca7f72280680c858108285c4
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-08-18 09:02:45 +0800

    change the display count

--- select.sql
+++ select.sql
@@ -1,5 +1,7 @@
-select * from student limit 20,10;
+select * from student limit 19,11;
 
 select class.class_name,count(*) as 'sum' from student,class where student.class_id = class.class_id group by student.class_id;
 
 select class.class_name,student.sex,count(*) as 'sum' from student,class where student.class_id = class.class_id group by student.class_id,student.sex;
+
+select * from schedule_detail;
